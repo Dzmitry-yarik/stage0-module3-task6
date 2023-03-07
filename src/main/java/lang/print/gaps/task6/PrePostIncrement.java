ab6a31a7545a66708bb5e585ea96ee2d7d97b06c
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-07 20:10:40 +0300

    Update PrePostIncrement.java

--- src/main/java/lang/print/gaps/task6/PrePostIncrement.java
+++ src/main/java/lang/print/gaps/task6/PrePostIncrement.java
@@ -6,7 +6,7 @@ public class PrePostIncrement {
             int b = 7;
             int c = 12;
             int d = 15;
-            int x =   ++a   + --b +   ++c   + d;
+            int x =   a++   + b +   ++c   + d;
             System.out.println("Expected x is 40, x = " + x);
             System.out.println("The updated value of a = " + a + ", b = " + b + ", c = " + c + " and d = " + d);
     }
